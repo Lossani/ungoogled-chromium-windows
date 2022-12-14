307ab381fa20588143144cbf8fa612e58756e498
Leonardo Molina <58919471+Lossani@users.noreply.github.com>
2022-12-14 16:45:45 -0500

    Update build.py

--- build.py
+++ build.py
@@ -227,7 +227,7 @@ def main():
     # Run ninja
     if args.ci:
         _run_build_process_timeout('third_party\\ninja\\ninja.exe', '-C', 'out\\Default', 'chrome',
-                                   'chromedriver', 'mini_installer', timeout=6*60*60)
+                                   'chromedriver', 'mini_installer', timeout=5.5*60*60)
         # package
         os.chdir(_ROOT_DIR)
         subprocess.run([sys.executable, 'package.py'])
